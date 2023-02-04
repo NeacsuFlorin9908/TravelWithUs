b9f293a8e4d0943739b0782647d4fbf6878dd84e
Florin Neacsu <florin.florin9908@gmail.com>
2023-02-04 22:33:37 +0200

    Changes

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function verifyPassword(){
     const confirmPassword = document.getElementById("confirm_passwordInput");
     const createButton = document.getElementById("createButton");
     const matchingTxt = document.getElementById("matching");
+    const nameInput = document.getElementById("nameInput");
+    const nameValue = document.getElementById("nameValue");
     if (confirmPassword.value) {
     if (password.value != confirmPassword.value) {
        matchingTxt.style.display = 'block';
        matchingTxt.style.color = 'red';
        matchingTxt.innerHTML = 'Not Matching';
-       
        return false;
        
        
@@ -18,6 +19,11 @@ function verifyPassword(){
         matchingTxt.style.display = 'block';
         matchingTxt.style.color = 'green';
         matchingTxt.innerHTML = 'Matching';
+       // let name = new Object();
+       // name.nameInput = nameInput.value;
+        //nameValue.innerText = name.nameInput;
+        //console.log(name.nameInput);
+       // console.log(nameValue);
         return true;
     }
     } 
